59e7f9e5c45c5f0c63fd245371ef2cf362a9e1bb
JavanAppleby <styx_n_stonz@hotmail.com>
2021-11-29 19:03:01 +1000

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solution here
+function findMatching(drivers, name) {
+    return drivers.filter( find =>
+        find.toLowerCase() === name.toLowerCase()
+        )
+}
+
+function fuzzyMatch(drivers, name) {
+    return drivers.filter ( find =>
+        find.toLowerCase().indexOf(name.toLowerCase()) === 0
+        )
+}
+
+function matchName(drivers, name) {
+    return drivers.filter( list => list.name === name)
+}
